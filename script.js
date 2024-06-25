c2786c3cb318e81fefafb426c52f0e085ba16799
Lícia Barreto <barretolicia2301@hotmail.com>
2024-06-25 15:34:05 -0300

    Responsivo p1
    Primeira parte do responsivo

--- script.js
+++ script.js
@@ -3,22 +3,36 @@ let sections = document.querySelectorAll('.section');
 
 window.addEventListener('scroll',()=>{
   sections.forEach(section => {
-    let top = window.scrollY;
+    var top = window.scrollY;
     let offset = section.offsetTop - 50;
     let heightSection = section.offsetHeight;
     let idSection = section.getAttribute('id')
     
+    console.log(top)
     if((top >= offset) && (top < offset + heightSection)){
       links.forEach(link => {
         link.classList.remove('ativo')
 
-        document.querySelector(`nav a[href*="${idSection}"]`).classList.add('ativo')
+         document.querySelector(`nav a[href*="${idSection}"]`).classList.add('ativo');
+         let hrefatual = document.querySelector(`nav a[href*="${idSection}"]`).getAttribute('href');
+         //console.log(hrefatual)
       })
     }
-    
+    if(top == 0){
+      resetaranimation();
+    }
   })
 })
 
+function resetaranimation(){
+  let home = document.querySelector('#typewriter');
+  console.log(home)
+  home.style = "animation: none"
+
+  requestAnimationFrame(() => {
+    home.style = "animation: typing 2.5s steps(15),    blink-caret .4s step-end infinite alternate;"
+  })
+}
 
 function scrollSection(event){
   event.preventDefault();
@@ -26,6 +40,10 @@ function scrollSection(event){
   const href = event.currentTarget.getAttribute('href');
   const section = document.querySelector(href)
   let topSection = section.offsetTop;
+
+  if(href=='#home'){
+    resetaranimation();
+  }
 
   window.scrollTo({
     top: topSection,
@@ -36,3 +54,4 @@ function scrollSection(event){
 links.forEach(link => {
   link.addEventListener('click', scrollSection)
 })
+
